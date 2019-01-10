92da66298ede77f722d41dad65d898099c1185e5
satyasinha94 <satyasinha94@gmail.com>
2019-01-10 15:16:00 -0500

    Done

--- index.js
+++ index.js
@@ -5,3 +5,43 @@ const questions = [
 ]
 
 let question;
+const questionContainer = document.querySelector('.question-container')
+const trueFalsebtns = Array.from(document.querySelector('.true-false-list').children)
+const questionBtn =  document.querySelector('a')
+
+function appendQuestion (question) {
+  questionContainer.innerHTML = question.questionText
+}
+
+function askQuestionThen (time) {
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(function(resolve) {
+    setTimeout(() => resolve(), time)
+  })
+}
+
+function removeQuestion () {
+  questionContainer.innerHTML = ""
+}
+
+function askQuestionThenRemoveQuestion (time) {
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons () {
+  return trueFalsebtns
+}
+
+function toggleTrueAndFalseButtons () {
+  trueFalsebtns.forEach(function(btn) {
+    btn.classList.toggle('hide')
+  })
+}
+
+function displayQuestionOnClick () {
+    questionBtn.addEventListener('click', function(e){
+    askQuestionThenRemoveQuestion(5000)
+    toggleTrueAndFalseButtons()
+  })
+}
