788c2e25a4fe353f3d81ead5ed3c5f4b0c32257e
dhamilt9 <danhamilton16@gmail.com>
2019-01-10 13:53:45 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,56 @@ const questions = [
 ]
 
 let question;
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion);
+}
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector('a.btn').addEventListener("click", function(){
+    displayQuestionOnClick()
+  })
+})
+
+function displayQuestionOnClick(){
+  buttons=trueAndFalseButtons();
+  buttons.forEach(function(button){
+    if (button.classList.contains("hide")){
+      button.classList.remove("hide")
+    }
+  });
+  askQuestionThenRemoveQuestion(5000).then(resp => toggleTrueAndFalseButtons(true))
+}
+function appendQuestion(question){
+  document.querySelector('.question-container').innerHTML=`${question.questionText}`
+}
+
+function askQuestionThen(time){
+  question=questions[0]
+  appendQuestion(question)
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      resolve("")
+    }, time )
+  })
+}
+function removeQuestion(){
+  document.querySelector('.question-container').innerHTML=``
+}
+
+function trueAndFalseButtons(){
+  return document.querySelectorAll('.lighten-2')
+}
+
+function toggleTrueAndFalseButtons(log=false){
+  if (log){
+    console.log("Toggling");
+  }
+  buttons=trueAndFalseButtons();
+  buttons.forEach(function(button){
+    if (button.classList.contains("hide")){
+      button.classList.remove("hide")
+    }else{
+      button.classList.add("hide")
+    }
+  });
+}
