8474a4bae2e6f4c6ec01b5c242757ab2bad5574e
lumrachele <rlum894@gmail.com>
2019-01-10 22:57:20 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,59 @@ const questions = [
 ]
 
 let question;
+const questionContainer = document.querySelector(".question-container")
+
+function appendQuestion(question){
+  questionContainer.innerText = question.questionText
+
+}
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise(function(resolve){
+    setTimeout(function(question){
+      resolve(question)
+    }, time*10)
+  })
+  }
+
+
+const removeQuestion = function(){
+  return questionContainer.innerText = ""
+}
+
+function askQuestionThenRemoveQuestion(time){
+// should append question for given time
+  return askQuestionThen(time)
+// then remove question when promise resolves
+ .then(removeQuestion)
+ .then(toggleTrueAndFalseButtons)
+}
+
+function trueAndFalseButtons(){
+  // return btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+
+  return btns= document.querySelectorAll(`[data-id]`)
+  // return btns
+}
+
+function toggleTrueAndFalseButtons(){
+  // debugger
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle("hide")
+  })
+}
+
+function displayQuestionOnClick(){
+// displays the question for after
+// a click event to the button
+// so we have to find the button
+// and put an event listener on it
+  askButton = document.querySelector(".waves-effect")
+
+  askButton.addEventListener("click", (e)=>{
+
+
+  })
+}
