5802e4dfb4fd9ad7faf1b5819b9c3011bfe7868f
isaac <oseasisaac91@gmail.com>
2019-01-11 14:49:43 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,56 @@ const questions = [
 ]
 
 let question;
+
+const questionContainer = document.querySelector('.question-container')
+
+document.addEventListener('DOMContentLoaded', () => {
+  questionContainer.innerHTML = question
+
+
+})
+
+function appendQuestion(questionAdd){
+  const questionContainer = document.querySelector('.question-container')
+  questionContainer.innerHTML = questionAdd.questionText
+
+}
+
+function askQuestionThen(time){
+  question = questions[0]
+  return new Promise(function(resolve){
+    setTimeout(() => resolve(), time)
+
+  })
+}
+
+function removeQuestion(){
+  questionContainer.innerHTML = ''
+}
+
+function askQuestionThenRemoveQuestion(time){
+  appendQuestion(questions[0])
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons(){
+  return document.querySelectorAll('.lighten-2')
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(function(btn) {
+    if(btn.className.includes('hide')){
+      btn.classList.remove('hide')
+    } else {
+      btn.classList.add('hide')
+    }
+  })
+}
+
+function displayQuestionOnClick(){
+  document.querySelector('a.btn').addEventListener('click', (event) => {
+   toggleTrueAndFalseButtons()
+   askQuestionThenRemoveQuestion(5000)
+   toggleTrueAndFalseButtons()
+  })
+}
