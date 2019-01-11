dabc26671d06d1f7d71634c4c8c8def0e94867b5
MichaelM3 <dagreekarican@aim.com>
2019-01-11 13:00:56 -0500

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,46 @@ const questions = [
   {questionText: "Humans can distinguish between over one trillion different smells", answer: true},
   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
 ]
+const questionContainer = document.querySelector('.question-container')
+const buttons = document.querySelector('.true-false-list').querySelectorAll('.btn')
 
 let question;
+
+function appendQuestion(question) {
+  questionContainer.innerHTML = question.questionText
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      resolve()
+    }, time)
+  })
+}
+
+function removeQuestion() {
+  return new Promise( () => {
+    questionContainer.innerHTML = ""
+  })
+}
+
+function askQuestionThenRemoveQuestion(time) {
+   return askQuestionThen(time).then(removeQuestion)
+}
+
+function trueAndFalseButtons() {
+  return buttons
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach( (button) => {
+  button.classList.toggle("hide");
+  })
+}
+
+function displayQuestionOnClick(time) {
+  toggleTrueAndFalseButtons()
+  askQuestionThenRemoveQuestion(time)
+}
